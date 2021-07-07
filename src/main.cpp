a7189cfb8bbc9aa61821f0563062780293a49348
Jeremy Parker <jeremyparker@pop-os.localdomain>
2021-07-07 20:30:17 +1200

    add ImGui to project

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
+#include "../lib/imgui/imgui.h"
+#include "../lib/imgui/imgui_impl_glfw.h"
+#include "../lib/imgui/imgui_impl_opengl3.h"
 
-GLFWwindow* initialize()
+GLFWwindow *initialize()
 {
     int glfwInitRes = glfwInit();
     if (!glfwInitRes)
@@ -16,8 +19,8 @@ GLFWwindow* initialize()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "CryptoWatch", nullptr, nullptr);
-    if (!window) 
+    GLFWwindow *window = glfwCreateWindow(1280, 720, "CryptoWatch", nullptr, nullptr);
+    if (!window)
     {
         std::cout << "Unable to create GLFW window" << std::endl;
         glfwTerminate();
@@ -34,30 +37,80 @@ GLFWwindow* initialize()
         return nullptr;
     }
     return window;
-
 }
-
-
 
 int main()
 {
-    GLFWwindow* window = initialize();
-   if (!window) {
-      return 0;
-   }
+    GLFWwindow *window = initialize();
+    if (!window)
+    {
+        return 0;
+    }
 
-   // Set the clear color to a nice green
-   glClearColor(0.15f, 0.6f, 0.4f, 1.0f);
+    // Set the clear color to a nice green
+    glClearColor(0.15f, 0.6f, 0.4f, 1.0f);
+    std::string glsl_version = "#version 150";
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    (void)io;
 
-   while (!glfwWindowShouldClose(window)) {
-      glClear(GL_COLOR_BUFFER_BIT);
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version.c_str());
 
-      glfwSwapBuffers(window);
-      glfwPollEvents();
-   }
+    bool my_tool_active = true;
 
-   glfwDestroyWindow(window);
-   glfwTerminate();
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
 
-   return 0;
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // Create a window called "My First Tool", with a menu bar.
+        ImGui::Begin("My First Tool", &my_tool_active, ImGuiWindowFlags_MenuBar);
+        if (ImGui::BeginMenuBar())
+        {
+            if (ImGui::BeginMenu("File"))
+            {
+                if (ImGui::MenuItem("Open..", "Ctrl+O"))
+                { /* Do stuff */
+                }
+                if (ImGui::MenuItem("Save", "Ctrl+S"))
+                { /* Do stuff */
+                }
+                if (ImGui::MenuItem("Close", "Ctrl+W"))
+                {
+                    my_tool_active = false;
+                }
+                ImGui::EndMenu();
+            }
+            ImGui::EndMenuBar();
+        }
+
+        // Plot some values
+        const float my_values[] = {0.2f, 0.1f, 1.0f, 0.5f, 0.9f, 2.2f};
+        ImGui::PlotLines("Frame Times", my_values, IM_ARRAYSIZE(my_values));
+
+        // Display contents in a scrolling region
+        ImGui::TextColored(ImVec4(1, 1, 0, 1), "Important Stuff");
+        ImGui::BeginChild("Scrolling");
+        for (int n = 0; n < 50; n++)
+            ImGui::Text("%04d: Some text", n);
+        ImGui::EndChild();
+        ImGui::End();
+
+        // rendering
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
 }
