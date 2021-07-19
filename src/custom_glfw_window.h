2370c04d133de7cb3e172c4d53b920d6754f41c7
Jeremy Parker <jeremyparker@pop-os.localdomain>
2021-07-20 09:07:40 +1200

    add comments to glfw custom window class

--- src/custom_glfw_window.h
+++ src/custom_glfw_window.h
@@ -3,13 +3,19 @@
 class CustomGLFWWindow {
     public:
         CustomGLFWWindow(int width, int height) : _width(width), _height(height) {};
+        // Initialize GLFW window
         void CreateWindow();
         // window uses glfwCreateWindow which uses malloc, therefore not using a smart pointer.
         GLFWwindow *window = nullptr;
+        // Clear GLFW window each frame before rendering next frame
         void ClearWindow() const;
+        // Check if window should close
         bool WindowShouldClose();
+        // Swap buffers and poll events
         void EndOfRunLoopWindowFunctions();
+        // Clean up window on exit
         void DetachGLFWWindow();
+        // Get GLFW window dimensions (width x height)
         std::pair<int, int> GetWindowDimensions();
     private:
         int _width;
